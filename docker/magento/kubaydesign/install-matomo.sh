fa0c145f37a4f2cf89224516688ca575eaaceb76
Mykola Kikets <mykola.kikets@gmail.com>
2023-04-24 09:00:39 +0300

    Update install-matomo.sh
    changed projects_cmd to full command

--- docker/magento/kubaydesign/install-matomo.sh
+++ docker/magento/kubaydesign/install-matomo.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
-. prepare.sh
+read -p "Path to magento folder: " path
+cd $path
 mkdir -p magento_data/app/code/Chessio
 if ! command -v git &> /dev/null
 then
     dnf install git
 fi
 git clone https://github.com/fnogatz/magento2-matomo.git magento_data/app/code/Chessio/Matomo
-docker compose exec --workdir /bitnami/magento magento "chown -R 1:0 ../magento"
-projects_cmd magento m-soft-regenerate
+echo "Restart"
+docker compose exec --workdir /bitnami/magento magento chown -R 1:0 ../magento
+docker compose restart magento
+#printf "Waiting 60 sec"
+#sleep 60
+read -p "Waiting for input..."
+#until [[ "$(docker logs --tail 1 $(docker ps  --format "{{.Names}}" | grep $(basename $(pwd))) 2>&1 | grep -m 1 "FOREG$
+echo -e "\nContinue\nStatic Deploy:"
+#docker compose exec --workdir /bitnami/magento magento bin/magento setup:static-content:deploy -f en_US $3
+./static-gen.sh
+echo "Permission fix:"
+docker compose exec --workdir /bitnami/magento magento chown -R 1:0 ../magento
